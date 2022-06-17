e79d0c62dbff73e2892db30e63e1caebfad9e54a
Edgar018 <edgar_belandria010@hotmail.com>
2022-06-17 18:05:06 -0400

    Getting started playwright

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM python:3.8-alpine
+FROM python:3.8
+FROM mcr.microsoft.com/playwright/python:v1.22.0-focal
+RUN pip install --upgrade pip
 RUN mkdir /app
 ADD . /app
 WORKDIR /app
--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
-def init():
-    print("Hello World")
+import asyncio
+from playwright.async_api import async_playwright
+
+
+async def init():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
+        await page.goto("https://www.google.com/")
+        print(await page.title())
+        await browser.close()
 
 
 if __name__ == '__main__':
-    init()
+    asyncio.run(init())
