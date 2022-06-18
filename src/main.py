30e804e089a6b84ae3be20973250ff3e6b0af0d1
Edgar018 <edgar_belandria010@hotmail.com>
2022-06-18 19:18:03 -0400

    check stock of microsoft

--- src/main.py
+++ src/main.py
@@ -2,12 +2,29 @@ import asyncio
 from playwright.async_api import async_playwright
 
 
+def checkStock(ContentButton):
+    if ContentButton == "Finalizar la compra":
+        return True
+    else:
+        return False
+
+
+shops = [
+    {
+        'shop': 'Microsoft',
+        'url': 'https://www.xbox.com/es-es/configure/8WJ714N3RBTL',
+    },
+]
+
+
 async def init():
     async with async_playwright() as p:
         browser = await p.chromium.launch()
         page = await browser.new_page()
-        await page.goto("https://www.google.com/")
-        print(await page.title())
+        await page.goto("https://www.xbox.com/es-es/configure/8WJ714N3RBTL")
+        content = await page.text_content('[aria-label="Finalizar la compra del pack"]')
+        has_stock = checkStock(content)
+        print({'shop': 'Microsoft', 'content': content, 'has_stock': has_stock})
         await browser.close()
 
 
